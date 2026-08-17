c7d7667bb63d76444cf98e362b36f43e79403aca
benylwang <102wangyingli@163.com>
2026-08-17 16:14:20 +0800

    del demo

--- esp32-gamepad-hid-demo/scripts/build.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-$ErrorActionPreference = "Stop"
-
-$env:IDF_TOOLS_PATH = "C:\Users\Administrator\.espressif"
-. "C:\esp\v6.0.1\esp-idf\export.ps1"
-
-idf.py `
-  -D CMAKE_MAKE_PROGRAM=C:\Users\Administrator\.espressif\tools\ninja\1.12.1\ninja.exe `
-  build
